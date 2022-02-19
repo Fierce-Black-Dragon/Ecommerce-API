c3afc7cdd9a4ae2d13dfc808cd9c1bd9fbd68db2
Fierce-Black-Dragon <blackdragon4204@gmail.com>
2022-02-19 23:08:50 +0530

    added create product feature in controller

--- controller/productController.js
+++ controller/productController.js
@@ -1,6 +1,50 @@
+const cloudinary = require("cloudinary").v2;
+const Product = require("../model/Product");
+const createError = require("http-errors");
+
 exports.testProduct = async (req, res, next) => {
   res.status(200).json({
     success: true,
     message: "test product successfully fetch",
   });
+};
+exports.createProduct = async (req, res, next) => {
+  try {
+    //array of  for product images
+    const productImageArrays = [];
+    // checking if req.file (image is present .)
+    if (!req.files) {
+      throw createError.BadRequest("products photo are required");
+    }
+    // if req. files
+    if (req.file.photos) {
+      const photos = req.file.photos;
+      console.log(photos);
+      for (let index = 0; index < photos.length; index++) {
+        responseImage = await cloudinary.uploader.upload(
+          photos[index].tempFilePath,
+          {
+            folder: "products",
+          }
+        );
+
+        imageArray.push({
+          id: responseImage.public_id,
+          secure_url: responseImage.secure_url,
+        });
+      }
+    }
+    req.body.photos = imageArray;
+    req.body.user = req.user.id;
+
+    const product = await Product.create(req.body);
+
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
