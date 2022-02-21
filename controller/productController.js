746a77022f3c1bdeae43978edf65c2f6f6b9cd4e
Fierce-Black-Dragon <blackdragon4204@gmail.com>
2022-02-21 20:29:23 +0530

    improved error handling

--- controller/productController.js
+++ controller/productController.js
@@ -1,43 +1,57 @@
 const cloudinary = require("cloudinary").v2;
 const Product = require("../model/Product");
 const createError = require("http-errors");
+const Category = require("../model/Category");
 
-exports.testProduct = async (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    message: "test product successfully fetch",
-  });
-};
+//create product
 exports.createProduct = async (req, res, next) => {
   try {
+    // getting the require data from req.body
+    const { name, price, description, category, stock, brand } = req.body;
+
+    //if any fields are missing
+    if (!(name && price && description && category && stock && brand)) {
+      throw createError.BadRequest("all fields  are required");
+    }
     //array of  for product images
     const productImageArrays = [];
     // checking if req.file (image is present .)
     if (!req.files) {
       throw createError.BadRequest("products photo are required");
     }
+
     // if req. files
-    if (req.file.photos) {
-      const photos = req.file.photos;
-      console.log(photos);
-      for (let index = 0; index < photos.length; index++) {
+    if (req.files.photos) {
+      for (let index = 0; index < req.files.photos.length; index++) {
         responseImage = await cloudinary.uploader.upload(
-          photos[index].tempFilePath,
+          req.files.photos[index].tempFilePath,
           {
             folder: "products",
           }
         );
 
-        imageArray.push({
+        productImageArrays.push({
           id: responseImage.public_id,
           secure_url: responseImage.secure_url,
         });
       }
     }
-    req.body.photos = imageArray;
-    req.body.user = req.user.id;
 
-    const product = await Product.create(req.body);
+    const ifCategoryExists = await Category.findById(category);
+    if (!ifCategoryExists) {
+      throw createError.NotFound("unknown category received");
+    }
+
+    const product = await Product.create({
+      name: name,
+      price: price,
+      description: description,
+      category: category,
+      stock: stock,
+      brand: brand,
+      photos: productImageArrays,
+      user: req.user._id,
+    });
 
     res.status(200).json({
       success: true,
