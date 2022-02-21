0102a2ea10a044a74df79aa9e8439f74fc2c8f3f
Fierce-Black-Dragon <blackdragon4204@gmail.com>
2022-02-21 20:46:04 +0530

    add schema vadilation for poduct creation

--- utils/schemaValidator.js
+++ utils/schemaValidator.js
@@ -1,5 +1,5 @@
 const Joi = require("joi");
-
+//auth schemas
 const authRSchema = Joi.object({
   name: Joi.string().alphanum().min(5).max(30).required(),
 
@@ -17,9 +17,21 @@ const resetPasswordSchema = Joi.object({
   password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
   confirmPassword: Joi.ref("password"),
 });
+
+//product schema
+const productValidationSchema = Joi.object({
+  name: Joi.string().alphanum().min(5).max(120).required(),
+  price: Joi.number().integer().required(),
+  stock: JoiJoi.number().integer().required(),
+  description: Joi.string().required(),
+  category: Joi.string().required(),
+  brand: Joi.string().required(),
+});
+
 module.exports = {
   authLSchema,
   authRSchema,
   resetPasswordSchema,
   forgotSchema,
+  productValidationSchema,
 };
