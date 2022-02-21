8c0fbbeda3f2980ab823f4411410fd5904e20df3
Fierce-Black-Dragon <blackdragon4204@gmail.com>
2022-02-21 20:48:24 +0530

    category creation is for admin and manger role only

--- routes/category.js
+++ routes/category.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { createCategory } = require("../controller/categoryController");
-
+const { isLoggedIn, customRoleChecker } = require("../middleware/authVerify");
+const { admin, manager, seller, user } = require("../config/roles");
 //all category  routes
-router.route("/createC").post(createCategory);
+router
+  .route("/createC")
+  .post(isLoggedIn, customRoleChecker(admin, manager), createCategory);
 //TODO: get categories route
 
 //exports home routes
