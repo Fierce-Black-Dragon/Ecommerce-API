255f368271a867637fdc3e3d1fbb11e15f223858
Fierce-Black-Dragon <blackdragon4204@gmail.com>
2022-02-21 20:49:44 +0530

    product creation is for admin , manger and seller role only

--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { testProduct } = require("../controller/productController");
-
+const { createProduct } = require("../controller/productController");
+const { admin, manager, seller, user } = require("../config/roles");
+const { isLoggedIn, customRoleChecker } = require("../middleware/authVerify");
 //all home routes
-router.route("/testproduct").get(testProduct);
+router
+  .route("/createProduct")
+  .post(isLoggedIn, customRoleChecker(admin, manager, seller), createProduct);
 
 //exports home routes
 module.exports = router;
