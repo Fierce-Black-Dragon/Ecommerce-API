7bb31e8497a142c5ea2771ca34f7192a38c1bae0
Fierce-Black-Dragon <blackdragon4204@gmail.com>
2022-03-12 00:57:07 +0530

    added cart model

--- model/ShippingAddress.js
+++ model/ShippingAddress.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const shippingAddressSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
