e51276cf394dd12cf55a265820352cee7f768856
Fierce-Black-Dragon <blackdragon4204@gmail.com>
2022-03-12 01:04:47 +0530

    add a shipping addres

---
+++ controller/shippingAddressController.js
@@ -0,0 +1,29 @@
+const ShippingAddress = require("../model/ShippingAddress");
+const createError = require("http-errors");
+exports.createShippingAddress = async (req, res, next) => {
+  try {
+    const { fullName, addressLine, city, postalCode, country, contact_no } =
+      req.body;
+    if (
+      !(fullName && addressLine && city && postalCode && country && contact_no)
+    ) {
+      throw createError.BadRequest(" all felids required");
+    }
+    const address = await ShippingAddress.create({
+      user: req.user._id,
+      fullName,
+      addressLine,
+      city,
+      postalCode,
+      country,
+      contact_no,
+    });
+    res.status(201).json({
+      success: true,
+      message: "ShippingAddress added successfully",
+      ShippingAddress: address,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
